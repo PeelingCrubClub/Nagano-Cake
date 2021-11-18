9ddfa1e87b2290956654a0c2bf4f8540e807b4ec
mapy0 <90832266+mapy0@users.noreply.github.com>
2021-11-18 12:49:20 +0000

    [fix]rous

--- config/routes.rb
+++ config/routes.rb
@@ -1,76 +1,75 @@
 Rails.application.routes.draw do
-  
+
   #管理者
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-    sessions: 'admins/sessions',
+  sessions: 'admins/sessions',
   }
-  
+
   namespace :admin do
   root to: 'homes#top'
-  
+
   #items
-  resources :items, only:[:new, :show, :index, :edit, :create, :update]  
-  
+  resources :items, only:[:new, :show, :index, :edit, :create, :update]
+
   #genres
-  resources :genres, only:[:index, :edit, :create, :update] 
-  
+  resources :genres, only:[:index, :edit, :create, :update]
+
   #customers
   resources :customers, only:[:index, :edit, :show, :update]
-  
+
   #orders
   resources :orders, only:[:show, :update] do
      #oder_items
      resources :order_items, only:[:update]
   end
-  
+
   end
-  
-    
+
+
   #会員
   devise_for :customers,skip: [:passwords,], controllers: {
     registrations: 'customers/registrations',
     sessions: 'customers/sessions',
   }
-  
+
   scope module: :public do
-  
+
   #homes
-  root to: 'homes#top'
-  get 'homes#about'
-  
+  root 'homes#top'
+  get 'homes/about' => 'homes#about'
+
   #items
   resources :items, only:[:show, :index]
-  
+
   #cart_items
   resources :cart_items, only:[:index, :create, :update, :destroy] do
-    collection do 
+    collection do
     delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
     end
   end
-   
+
   #orders
    resources :orders, only:[:new, :show, :index, :create] do
-     collection do 
+     collection do
      post 'orders/confirm' => 'orders#confirm'
      get 'orders/complete' => 'orders#complete'
      end
    end
-   
+
   #customers
    resources :customers, only:[:show, :edit, :update] do
-     collection do 
+     collection do
      get 'customers/leave' => 'customers#leave'
      patch 'customers/out' => 'customers#out'
      end
-   
+
    end
-  
+
   #deliveries
-  resources :deliveries, only:[:index, :create, :edit, :update, :destroy]  
-  
-    
+  resources :deliveries, only:[:index, :create, :edit, :update, :destroy]
+
   end
 
- 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
