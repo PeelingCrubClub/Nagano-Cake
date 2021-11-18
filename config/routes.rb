8e8057c7b294db1f05a78f77639f23819802c2f7
mapy0 <90832266+mapy0@users.noreply.github.com>
2021-11-18 08:06:21 +0000

    [in middle of] routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,71 @@
 Rails.application.routes.draw do
+  
+  #管理者
   devise_for :admins, controllers:{
     sessions: 'admins/sessions',
     passwords: 'admins/passwords',
   }
+  
+  namespace :admin do
+  root to: 'homes#top'
+  
+  #items
+  resouces :items, only:[:new, :show, :index, :edit, :create, :update]  
+  
+  #genres
+  resouces :genres, only:[:index, :edit, :create, :update] 
+  
+  #customers
+  resouces :customers, only:[:index, :edit, :show, :update]
+  
+  #orders
+  resouces :orders, only:[:show, :update]
+  
+  #oder_items
+  resouces :oder_items, only:[:update]
+  
+  end
+  
     
+  #会員
   devise_for :customers, controllers:{
     registrations: 'customers/registrations',
     sessions: 'customers/sessions',
     passwords: 'customers/passwords',
   }
+  
+  scope module: :public do
+  
+  #homes
+  root to: 'homes#top'
+  get 'homes#about'
+  
+  #items
+  resouces :items, only:[:show, :index]
+  
+  #cart_items
+  resouces :cart_items, only:[:index, :create, :update, :destroy] do
+  delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
+  end
+   
+  #orders
+   resouces :orders, only:[:new, :show, :index, :create] do
+   post 'orders/confirm' => 'orders#confirm'
+   get 'orders/complete' => 'orders#complete'
+   end
+   
+  #customers
+   resouces :customers, only:[:show, :edit, :update] do
+   get 'customers/leave' => 'customers#leave'
+   patch 'customers/out' => 'customers#out'
+   end
+  
+  #deliveries
+  resouces :deliveries, only:[:index, :create, :edit, :update, :destroy]  
+  
+    
+  end
 
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
