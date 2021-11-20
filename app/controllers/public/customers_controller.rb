2d74928e98b4a48e6bd821557ba64955cbda5f12
MasahiroTabata <79088468+masahirotabata@users.noreply.github.com>
2021-11-20 15:52:55 +0000

    feature1/customersコントローラーupdate

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,26 +1,18 @@
 class Public::CustomersController < ApplicationController
+   
+   before_action :ensure_correct_customer, {only: [:show, :edit]}
   
    def show
-      @customer = Customers.find_by(params[:id])
+      @customer = Customer.find_by(params[:id])
    end
    
    def edit
-      
-   @customer = Customers.find_by(params[:id])  
-   
-   unless @customer == current_customer
-   
-   render customer_edit_path(@customer.id), user:@user 
-   
-   end
-       
-      
-      
+   @customer = Customer.find(params[:id])
    end
    
    def update
       
-    @customer = @customer.update(params_customer)
+    @customer = @customer.update(customer_params)
    if @customer.update(customer_params)
     redirect_to customer_edit_path(current_user.id), notice: "You have updated user successfully."
     
@@ -34,8 +26,27 @@ class Public::CustomersController < ApplicationController
    
    
    def leave
+    @customer = Customer.find(params[:id])
    end
    
    def out
+   @customer = Customer.find(params[:id])
+       if @customer.update(is_enabled: false)
+          sign_out current_customer #Uコントローラーから直接サインアウト（devise公式)
+       end
+       redirect_to root_path
    end
+    
+  private
+  def customer_params
+  	  params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana,
+  	                                   :tel, :email, :password, :postal_code, :address)
+  end
+
+  def ensure_correct_customer
+    @customer = Customer.find(params[:id])
+    if current_customer.id != @customer.id
+       redirect_to root_path
+    end
+  end
 end
