37443d145caaa4fd1a5b6d53a72574846285b283
MasahiroTabata <79088468+masahirotabata@users.noreply.github.com>
2021-11-20 15:17:31 +0000

    feature1/customersコントローラー追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,13 +1,37 @@
 class Public::CustomersController < ApplicationController
   
    def show
+      @customer = Customers.find_by(params[:id])
    end
    
    def edit
+      
+   @customer = Customers.find_by(params[:id])  
+   
+   unless @customer == current_customer
+   
+   render customer_edit_path(@customer.id), user:@user 
+   
+   end
+       
+      
+      
    end
    
    def update
+      
+    @customer = @customer.update(params_customer)
+   if @customer.update(customer_params)
+    redirect_to customer_edit_path(current_user.id), notice: "You have updated user successfully."
+    
+   else
+   
+   render 'edit'
+    
    end
+   
+   end
+   
    
    def leave
    end
