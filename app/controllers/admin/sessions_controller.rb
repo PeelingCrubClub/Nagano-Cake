ad4fa45996ea933c8c34ca8e4ac03071abbe834c
mapy0 <90832266+mapy0@users.noreply.github.com>
2021-11-20 05:21:29 +0000

    admin device完了/ログイン後の遷移先指定/ルーティング指定

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -24,19 +24,19 @@ class Admin::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-  
+
   protected
     def after_sign_in_path_for(resource)
       admin_order_order_items_path
     end
-    
+
     def after_sign_out_path_for(resource)
       new_admin_session_path
     end
-    
+
     def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
     end
-  
+
 end
 
