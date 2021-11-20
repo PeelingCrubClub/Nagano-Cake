9ba652794f3f77d5162bd6b98d75f98c6bb96ed1
Takihamu <89925520+Takihamu@users.noreply.github.com>
2021-11-20 12:25:20 +0000

    Add Devise

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,8 @@
 class Item < ApplicationRecord
-  
+
   has_many :carts, dependent: :destroy
   has_many :order_items, dependent: :destroy
-  belongs_to :genres
-  
+  belongs_to :genre
+  attachment :image
+
 end
