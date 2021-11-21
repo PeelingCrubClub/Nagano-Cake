8bfeb22ec3c2dd5f57ecaef489dba8a65af81c1f
masahirotabata <79088468+masahirotabata@users.noreply.github.com>
2021-11-21 22:44:46 +0900

    Revert "会員登録画面、会員情報編集画面、退会画面、customers_controller,deliveries_controllerの更新"

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,16 +1,9 @@
 class Public::ItemsController < ApplicationController
-
+  
   def index
-    @items = Item.all
-    @customer = current_customer
-    @genres = Genre.all
   end
-
+  
   def show
-    @item = Item.find(params[:id])
-    @cart_item = CartItem.new
-    @customer = current_customer
-    @genres = Genre.all
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,7 @@
 class Item < ApplicationRecord
-
+  
   has_many :carts, dependent: :destroy
   has_many :order_items, dependent: :destroy
   belongs_to :genres
-  attachment :image
-
-  # 商品価格に消費税を加える記述
-  def add_tax_price
-    (self.price * 1.10).round
-  end
+  
 end
