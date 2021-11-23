843f5e759cadd229be98974f40d094116d2ae73b
mapy0 <90832266+mapy0@users.noreply.github.com>
2021-11-23 02:30:41 +0000

    [middle]

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,19 +1,19 @@
 class Admin::OrderItemsController < ApplicationController
-  
+
   def update
-    @order_item =OrderItem.find(params[:id])
+    order_item =OrderItem.find(params[:id])
+    order = Order.find(params[:order_id])
    if
-	    @order_item.update(order_detail_params)
-		   redirect_to admin_order_path(@order), notice:"製作ステータスを更新しました"#非同期通信導入検討
+	    order_item.update(order_item_params)
+		   redirect_to admin_order_path(order), notice:"製作ステータスを更新しました"#非同期通信導入検討
    else
        render :show, alert: "ステータスを更新できませんでした"
    end
-		
   end
-  
+
   private
    def order_item_params
-      params.require(:order_item).permit(:production_status) 
+      params.require(:order_item).permit(:production_status)
    end
-  
+
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,7 +2,8 @@ class OrderItem < ApplicationRecord
 
   belongs_to :order
   belongs_to :item
-  
-  enum roduction_status: { unable: 0, witing: 1, working: 2, completed: 3 }
+
+  enum production_status: { unable: 0, waiting: 1, working: 2, completed: 3 }
+  enum order_status: { waiting_for_deposit: 0, confirmed_payment: 1, in_production: 2, ready_to_delivery: 3, done: 4 }
 
 end
