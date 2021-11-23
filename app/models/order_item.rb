28e0145043c8c5136fbd59f5061b2b6d313cfeb1
Takihamu <89925520+Takihamu@users.noreply.github.com>
2021-11-23 07:20:42 +0000

    変更

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -6,4 +6,5 @@ class OrderItem < ApplicationRecord
   enum production_status: { unable: 0, waiting: 1, working: 2, completed: 3 }
   enum order_status: { waiting_for_deposit: 0, confirmed_payment: 1, in_production: 2, ready_to_delivery: 3, done: 4 }
 
+
 end
