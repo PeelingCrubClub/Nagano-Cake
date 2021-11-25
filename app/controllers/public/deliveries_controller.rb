13248828c3135d1154960cd4137ea49dd12ae20d
masahirotabata <79088468+masahirotabata@users.noreply.github.com>
2021-11-25 14:37:24 +0000

    マイページ関連更新

--- app/controllers/public/deliveries_controller.rb
+++ app/controllers/public/deliveries_controller.rb
@@ -36,7 +36,7 @@ class Public::DeliveriesController < ApplicationController
 
   private
   def delivery_params
-      params.require(:delivery).permit(:last_name, :first_name, :delivery_postal_code, :address_name)
+      params.require(:delivery).permit(:last_name, :first_name, :delivery_postal_code, :address_name,:delivery_address)
   end
 end
 
